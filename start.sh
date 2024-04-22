2699760201dd3148ae44aa4af7ec2ada90e1d1de
AjDonna <89831798+AjDonna@users.noreply.github.com>
2024-04-22 19:58:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Advance
 fi
-cd /Advance-File-Share-Bot
+cd /Advance
 pip3 install -U -r requirements.txt
 echo "Bot Started...."
 python3 bot.py
